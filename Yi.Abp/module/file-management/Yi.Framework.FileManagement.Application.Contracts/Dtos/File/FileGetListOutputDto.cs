63ed26846805da9d0bdefb9e5d939f33f539fb03
wcg <weichen0716@163.com>
2026-01-28 12:02:55 +0800

    refactor(file-management): 移除 SizeHelper

--- Yi.Abp/module/file-management/Yi.Framework.FileManagement.Application.Contracts/Dtos/File/FileGetListOutputDto.cs
+++ Yi.Abp/module/file-management/Yi.Framework.FileManagement.Application.Contracts/Dtos/File/FileGetListOutputDto.cs
@@ -1,5 +1,5 @@
 using Volo.Abp.Application.Dtos;
-using Yi.Framework.FileManagement.Files;
+using Yi.Framework.FileManagement.Domain.Shared.Consts;
 
 namespace Yi.Framework.FileManagement.Application.Contracts.Dtos.File;
 
@@ -11,13 +11,13 @@ public class FileGetListOutputDto : EntityDto<Guid>
     /// <summary>
     /// 文件大小
     /// </summary>
+
     public long FileSize { get; set; }
 
     /// <summary>
     /// 可读文件大小
     /// </summary>
-    public string BeautifySize => SizeHelper.BeautifySize(FileSize);
-
+    public string BeautifySize => BeautifyFileSize(FileSize);
     /// <summary>
     /// 文件类型
     /// </summary>
@@ -32,4 +32,54 @@ public class FileGetListOutputDto : EntityDto<Guid>
     /// 创建时间
     /// </summary>
     public DateTime CreationTime { get; set; }
+
+    /// <summary>
+    /// 美化文件大小（将字节数转换为可读格式）
+    /// </summary>
+    /// <param name="size">文件大小（字节）</param>
+    /// <returns>格式化后的文件大小字符串</returns>
+    private static string BeautifyFileSize(long size)
+    {
+        if (size == 0 || size == 1)
+        {
+            return $"{size} Byte";
+        }
+
+        if (size >= FileManagementConsts.Terabyte)
+        {
+            var fixedSize = ((float)size / (float)FileManagementConsts.Terabyte);
+            return $"{FormatSize(fixedSize)} TB";
+        }
+
+        if (size >= FileManagementConsts.Gigabyte)
+        {
+            var fixedSize = ((float)size / (float)FileManagementConsts.Gigabyte);
+            return $"{FormatSize(fixedSize)} GB";
+        }
+
+        if (size >= FileManagementConsts.Megabyte)
+        {
+            var fixedSize = ((float)size / (float)FileManagementConsts.Megabyte);
+            return $"{FormatSize(fixedSize)} MB";
+        }
+
+        if (size >= FileManagementConsts.Kilobyte)
+        {
+            var fixedSize = ((float)size / (float)FileManagementConsts.Kilobyte);
+            return $"{FormatSize(fixedSize)} KB";
+        }
+
+        return $"{size} B";
+    }
+
+    /// <summary>
+    /// 格式化文件大小数字
+    /// </summary>
+    /// <param name="size">文件大小</param>
+    /// <returns>格式化后的字符串</returns>
+    private static string FormatSize(float size)
+    {
+        var s = $"{size:0.00}";
+        return s.EndsWith("00") ? ((int)size).ToString() : s;
+    }
 }
